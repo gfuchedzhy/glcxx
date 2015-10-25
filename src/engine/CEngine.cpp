cde41a938338696ade7a4bf5b89938f49f220b69
Grygoriy Fuchedzhy <grygoriy.fuchedzhy@gmail.com>
2015-10-25 11:11:57 +0200

    add openGL version logging

--- src/engine/CEngine.cpp
+++ src/engine/CEngine.cpp
@@ -3,8 +3,11 @@
  */
 
 #include "CEngine.hpp"
+#include "Log.hpp"
 
 CEngine::CEngine()
    : mWindow(sf::VideoMode(800, 600), APPNAME, sf::Style::Titlebar | sf::Style::Close)
 {
+   sf::ContextSettings settings = mWindow.getSettings();
+   Log::msg("openGL ", settings.majorVersion, '.', settings.minorVersion,  " version loaded");
 }
