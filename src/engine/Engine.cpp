a9f4e06586a9adac4e213c5f1552cc8e739a7352
Grygoriy Fuchedzhy <grygoriy.fuchedzhy@gmail.com>
2015-10-27 20:23:20 +0200

    add test drawing of some polygon

--- src/engine/Engine.cpp
+++ src/engine/Engine.cpp
@@ -4,7 +4,7 @@
 
 #include "Engine.hpp"
 #include "Log.hpp"
-#include "GL.hpp"
+#include "Program.hpp"
 
 CEngine::CEngine()
    : mWindow(sf::VideoMode(800, 600), APPNAME, sf::Style::Titlebar | sf::Style::Close)
@@ -15,6 +15,35 @@ CEngine::CEngine()
 
 void CEngine::run()
 {
+   static const GLfloat vertexBufferData[] = {
+      -0.5f,-0.5f, 0.0f,
+       0.5f,-0.5f, 0.0f,
+       0.5f, 0.5f, 0.0f,
+      -0.5f, 0.5f, 0.0f,
+   };
+   static const GLubyte indices[] = {0, 1, 3, 2};
+
+   CProgram p = CProgram::create(
+R"(\
+attribute vec3 vertex;
+void main()
+{
+   gl_Position.xyz = vertex;
+   gl_Position.w = 1.0;
+}
+)",
+R"(\
+void main()
+{
+   gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
+}
+)");
+
+   GLuint vertexBuffer;
+   gl(glGenBuffers, 1, &vertexBuffer);
+   gl(glBindBuffer, GL_ARRAY_BUFFER, vertexBuffer);
+   gl(glBufferData, GL_ARRAY_BUFFER, sizeof(vertexBufferData), vertexBufferData, GL_STATIC_DRAW);
+
    while (true)
    {
       // handle events
@@ -32,8 +61,21 @@ void CEngine::run()
       }
 
       // clear the buffers
-      glClear(GL_COLOR_BUFFER_BIT);
+      gl(glClear, GL_COLOR_BUFFER_BIT);
+      p.bind();
+      gl(glEnableVertexAttribArray, 0);
+      gl(glBindBuffer, GL_ARRAY_BUFFER, vertexBuffer);
+      gl(glVertexAttribPointer,
+         0, // ?
+         3, // size
+         GL_FLOAT, // type
+         GL_FALSE, // not normalized?
+         0, // stride
+         (void*)0 // offset
+         );
 
+      gl(glDrawElements, GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_BYTE, indices);
+      gl(glDisableVertexAttribArray, 0);
       // end the current frame (internally swaps the front and back buffers)
       mWindow.display();
    }
