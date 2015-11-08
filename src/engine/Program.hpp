d13d03dead00e4e6051a34a30593bcf78af1f443
Grygoriy Fuchedzhy <grygoriy.fuchedzhy@gmail.com>
2015-11-08 17:29:17 +0200

    implement proper logic of attach, detach and select

--- src/engine/Program.hpp
+++ src/engine/Program.hpp
@@ -57,7 +57,25 @@ class TProgram : public CProgramObject
       template<typename TName, int I = indexByName(TName{})>
       void set(typename std::tuple_element<I, tProgramInputTuple>::type input)
       {
-         std::get<I>(mProgramInput) = input;
+         auto& current = std::get<I>(mProgramInput);
+         if (input != current)
+         {
+            if (isSelected())
+            {
+               // detach old input, attach new one
+               attach<I>(false);
+               current = input;
+               attach<I>(true);
+            }
+            else
+            {
+               // if attached and detach is not scheduled yet - schedule detach
+               auto& forDetach = std::get<I>(mProgramInputDetach);
+               if (mAttached[I] && !forDetach)
+                  forDetach = current;
+               current = input;
+            }
+         }
       }
 
       /// @brief @see CProgramObject::select
@@ -67,19 +85,53 @@ class TProgram : public CProgramObject
          doSelect(std::make_index_sequence<inputNum>{});
       }
 
-      /// @brief todo
+      /// @brief select impl
       template<size_t... I>
       void doSelect(std::index_sequence<I...>)
       {
-         swallow(std::get<I>(mProgramInput)->attach(std::get<I>(mLocations)));
+         swallow(doSelectSingle<I>());
+      }
+
+      /// @brief select impl for single program input
+      template<size_t I>
+      void doSelectSingle()
+      {
+         if (auto forDetach = std::get<I>(mProgramInputDetach))
+         {
+            forDetach->detach(std::get<I>(mLocations));
+            forDetach = nullptr;
+         }
+         if (auto input = std::get<I>(mProgramInput))
+         {
+            input->attach(std::get<I>(mLocations));
+            mAttached[I] = true;
+         }
       }
 
    private:
+      /// @brief if @param state true attach, otherwise detach
+      template<size_t I>
+      void attach(const bool state)
+      {
+         if (auto& input = std::get<I>(mProgramInput))
+         {
+            const auto& locations = std::get<I>(mLocations);
+            state ? input->attach(locations) : input->detach(locations);
+            mAttached[I] = state;
+         }
+      }
+
       /// @brief locations for all program input
       std::tuple<typename TProgramInput::tData::tLocations...> mLocations;
 
       /// @brief program input associated with program
       tProgramInputTuple mProgramInput;
+
+      /// @brief program input scheduled for detach
+      tProgramInputTuple mProgramInputDetach;
+
+      /// @brief true if program input is currently attached
+      std::array<bool, inputNum> mAttached = {}; // false
 };
 
 #endif // ENGINE_PROGRAM_HPP
