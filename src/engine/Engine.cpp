adf442d2d5d56189bdfe68d0b92534efd322c419
Grygoriy Fuchedzhy <grygoriy.fuchedzhy@gmail.com>
2015-11-09 18:46:38 +0200

    move buffer object program input to separate header

--- src/engine/Engine.cpp
+++ src/engine/Engine.cpp
@@ -5,7 +5,7 @@
 #include "Engine.hpp"
 #include "Log.hpp"
 #include "Program.hpp"
-#include "BufferObject.hpp"
+#include "BufferObjectProgramInput.hpp"
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
