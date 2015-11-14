24f7362e597e846e4a01f4a6f44d08f1c3feea6b
Grygoriy Fuchedzhy <grygoriy.fuchedzhy@gmail.com>
2015-11-14 17:35:28 +0200

    add CModel

---
+++ src/engine/Model.cpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2015 Grygoriy Fuchedzhy <grygoriy.fuchedzhy@gmail.com>
+ */
+
+#include "Model.hpp"
+#include <glm/gtc/matrix_transform.hpp>
+
+void CModel::pos(const glm::vec3& pos)
+{
+   if (pos != mPos)
+   {
+      mPos = pos;
+      mCache.mIsDirty = true;
+   }
+}
+
+void CModel::pitch(const float pitch)
+{
+   if (pitch != mPitch)
+   {
+      mPitch = pitch;
+      mCache.mIsDirty = true;
+   }
+}
+
+void CModel::roll(const float roll)
+{
+   if (roll != mRoll)
+   {
+      mRoll = roll;
+      mCache.mIsDirty = true;
+   }
+}
+
+void CModel::yaw(const float yaw)
+{
+   if (yaw != mYaw)
+   {
+      mYaw = yaw;
+      mCache.mIsDirty = true;
+   }
+}
+
+const glm::mat4& CModel::model() const
+{
+   if (mCache.mIsDirty)
+   {
+      mCache.mModel = glm::translate(glm::mat4{}, mPos);
+      mCache.mModel = glm::rotate(mCache.mModel, glm::radians(mYaw), glm::vec3{0.f, 0.f, 1.f});
+      mCache.mModel = glm::rotate(mCache.mModel, glm::radians(mPitch), glm::vec3{1.f, 0.f, 0.f});
+      mCache.mModel = glm::rotate(mCache.mModel, glm::radians(-mRoll), glm::vec3{0.f, 1.f, 0.f});
+      mCache.mIsDirty = false;
+   }
+   return mCache.mModel;
+}
--- src/engine/Model.hpp
+++ src/engine/Model.hpp
@@ -52,4 +52,74 @@ class IRenderableCachedModel : public IRenderableModel
       virtual void updateCachedModel(glm::mat4& cachedModel) const = 0;
 };
 
+class CModel
+{
+      /// @brief position
+      glm::vec3 mPos;
+
+      /// @brief pitch
+      float mPitch = 0.f;
+
+      /// @brief pitch
+      float mRoll = 0.f;
+
+      /// @brief yaw
+      float mYaw = 0.f;
+
+      /// @brief automatic cache
+      mutable struct SCache
+      {
+            glm::mat4 mModel;
+            bool mIsDirty = true;
+      } mCache;
+
+   public:
+      /// @brief sets pos
+      void pos(const glm::vec3& pos);
+
+      /// @brief returns pos
+      const glm::vec3& pos() const;
+
+      /// @brief sets pitch
+      void pitch(float pitch);
+
+      /// @brief returns pitch
+      float pitch() const;
+
+      /// @brief sets roll
+      void roll(float roll);
+
+      /// @brief returns roll
+      float roll() const;
+
+      /// @brief sets yaw
+      void yaw(float yaw);
+
+      /// @brief returns yaw
+      float yaw() const;
+
+      /// @brief returns model matrix
+      const glm::mat4& model() const;
+};
+
+inline const glm::vec3& CModel::pos() const
+{
+   return mPos;
+}
+
+inline float CModel::pitch() const
+{
+   return mPitch;
+}
+
+inline float CModel::roll() const
+{
+   return mRoll;
+}
+
+inline float CModel::yaw() const
+{
+   return mYaw;
+}
+
 #endif // ENGINE_MODEL_HPP
