bf78ffdc96af8ab4602a4e331bfaa8e00514b465
Grygoriy Fuchedzhy <grygoriy.fuchedzhy@gmail.com>
2015-11-15 15:08:46 +0200

    improve camera animation, add aircraft roll and pitch animation

--- src/app/App.cpp
+++ src/app/App.cpp
@@ -15,37 +15,49 @@ CApp::CApp()
    mCamera.pitch(30.f);
 }
 
+template<typename Get, typename Set>
+inline void animate(Get get, Set set,
+                    float timeDelta, float speed,
+                    sf::Keyboard::Key minKey, sf::Keyboard::Key maxKey,
+                    float minVal, float maxVal, bool dump = false)
+{
+   if (sf::Keyboard::isKeyPressed(minKey))
+      set(std::max(minVal, get() - timeDelta*speed));
+   else if (sf::Keyboard::isKeyPressed(maxKey))
+      set(std::min(maxVal, get() + timeDelta*speed));
+   else if (dump)
+   {
+      const float middle = 0.5f*(minVal+maxVal);
+      if (abs(get() - middle) > 0.001f)
+         set(middle + (get() - middle) * std::max(0.f, 1.f - timeDelta/0.2f));
+      else
+         set(middle);
+   }
+}
+
 void CApp::update(float timeDelta)
 {
    mAircraft.update(timeDelta);
 
-   const float cameraSpeed = 70.f;
-   if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-   {
-      mCamera.orientation(std::min(120.f, mCamera.orientation() + timeDelta*cameraSpeed));
-   }
-   if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-   {
-      mCamera.orientation(std::max(-120.f, mCamera.orientation() - timeDelta*cameraSpeed));
-   }
-   if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-   {
-      mCamera.pitch(std::min(90.f, mCamera.pitch() + timeDelta*cameraSpeed));
-   }
-   if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-   {
-      mCamera.pitch(std::max(-30.f, mCamera.pitch() - timeDelta*cameraSpeed));
-   }
+   animate([this]{return mCamera.orientation();},
+           [this](float val){mCamera.orientation(val);},
+           timeDelta, 70.f, sf::Keyboard::Left, sf::Keyboard::Right, -120.f, 120.f);
 
-   const float cameraEyeDistanceSpeed = 40.f;
-   if (sf::Keyboard::isKeyPressed(sf::Keyboard::Add))
-   {
-      mCamera.eyeDistance(std::max(30.f, mCamera.eyeDistance() - timeDelta*cameraEyeDistanceSpeed));
-   }
-   if (sf::Keyboard::isKeyPressed(sf::Keyboard::Subtract))
-   {
-      mCamera.eyeDistance(std::min(300.f, mCamera.eyeDistance() + timeDelta*cameraEyeDistanceSpeed));
-   }
+   animate([this]{return mCamera.pitch();},
+           [this](float val){mCamera.pitch(val);},
+           timeDelta, 70.f, sf::Keyboard::Down, sf::Keyboard::Up, -30.f, 90.f);
+
+   animate([this]{return mCamera.eyeDistance();},
+           [this](float val){mCamera.eyeDistance(val);},
+           timeDelta, 70.f, sf::Keyboard::Add, sf::Keyboard::Subtract, 30.f, 100.f);
+
+   animate([this]{return mAircraft.pitch();},
+           [this](float val){mAircraft.pitch(val);},
+           timeDelta, 70.f, sf::Keyboard::W, sf::Keyboard::S, -60.f, 60.f, true);
+
+   animate([this]{return mAircraft.roll();},
+           [this](float val){mAircraft.roll(val);},
+           timeDelta, 70.f, sf::Keyboard::A, sf::Keyboard::D, -60.f, 60.f, true);
 
    auto p = gRenderer.get<cts("coloredPolygon")>();
    p->set<cts("uViewProj")>(mCamera.viewProj());
